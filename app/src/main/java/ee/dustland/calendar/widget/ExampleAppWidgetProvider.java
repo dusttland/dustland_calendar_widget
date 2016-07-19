a192127fe7e8d40e9446a00b1a7194866fb61d10
Priit Trink <dusttland@gmail.com>
2016-07-19 13:06:08 +0300

    First commit.

---
+++ app/src/main/java/ee/dustland/calendar/widget/ExampleAppWidgetProvider.java
@@ -0,0 +1,39 @@
+package ee.dustland.calendar.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+/**
+ * Created by dustt on 17.7.16.
+ */
+public class ExampleAppWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        for (int i = 0; i < N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            String url = "http://dustland.ee";
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setData(Uri.parse(url));
+
+            PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_appwidget);
+
+            views.setOnClickPendingIntent(R.id.widget_button, pending);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+            Toast.makeText(context, "Widget added.", Toast.LENGTH_SHORT);
+
+        }
+    }
+}
